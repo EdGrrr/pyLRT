595b2f86fb151ab55c7f4db93bc3755d8c06a3fc
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2020-09-29 11:55:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup
 
-setup(name = 'pyLRT',
-      version = '0.1',
-      packages = ['pyLRT'])
+setup(name='pyLRT',
+      version='0.1',
+      author='Edward Gryspeerdt',
+      author_email='e.gryspeerdt@imperial.ac.uk',
+      maintainer='Edward Gryspeerdt',
+      maintainer_email='e.gryspeerdt@imperial.ac.uk',
+      description='A simple python interface/wrapper for LibRadTran',
+      license='BSD (3-clause)',
+      url='https://github.com/EdGrrr/pyLRT',
+      install_requires=['xarray',
+                        'numpy',
+                        'scipy'],
+      packages=['pyLRT'],)
