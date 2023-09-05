c64bb6fe0112fcfee636ee2deb67e441b86a47c4
Oliver <ogadriver147@gmail.com>
2023-09-05 10:57:28 +0100

    Fixes bug: "lambda" data variable persists

--- pyLRT/parser.py
+++ pyLRT/parser.py
@@ -40,6 +40,7 @@ def parse_output(output: ArrayLike, rt:RadTran, dims=["lambda"], **dim_specs):
         dim_values["wvl"] = dim_values["lambda"]
         del(dim_values["lambda"])
         dims[dims.index("lambda")] = "wvl"
+        output_cols[output_cols.index("lambda")] = "wvl"
         
     output_dataset = xr.DataArray(output, coords={**dim_values, "variable":output_cols}, dims=["variable"]+dims)
     output_dataset = output_dataset.to_dataset("variable")
